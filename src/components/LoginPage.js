0c383568883dee0972e8db5754605ec1214e247a
Jakie Rice <jakie.rice@gmail.com>
2017-11-28 10:31:05 -0600

    Modify loginPage styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
 	<div className="box-layout">
 		<div className="box-layout__box">
-			<h1 className="box-layout__title">React Boilerplate</h1>
-			<p>Application tagline.</p>
+			<h1 className="box-layout__title">Myce Cream</h1>
+			<p className="box-layout__tagline">Eat through your ice cream bucket list</p>
 			<button className="button" onClick={startLogin}>Login with Google</button>
 		</div>
 	</div>
